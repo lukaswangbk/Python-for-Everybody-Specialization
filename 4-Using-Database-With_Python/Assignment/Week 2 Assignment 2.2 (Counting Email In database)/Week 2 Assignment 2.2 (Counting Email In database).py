4cdb4f84f7dac2e6ca6652aa857367cc4c30682a
lukas Wang <lukaswang@users.noreply.github.com>
2021-01-25 19:08:26 +0800

    update

--- 4-Using-Database-With_Python/Assignment/Week 2 Assignment 2.2 (Counting Email In database)/Week 2 Assignment 2.2 (Counting Email In database).py
+++ 4-Using-Database-With_Python/Assignment/Week 2 Assignment 2.2 (Counting Email In database)/Week 2 Assignment 2.2 (Counting Email In database).py
@@ -28,11 +28,9 @@ import sqlite3
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
-cur.execute('''
-DROP TABLE IF EXISTS Counts''')
+cur.execute('''DROP TABLE IF EXISTS Counts''')
 
-cur.execute('''
-CREATE TABLE Counts (org TEXT, count INTEGER)''')
+cur.execute('''CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = input('Enter file name: ')
 if ( len(fname) < 1 ) : fname = 'mbox.txt'
